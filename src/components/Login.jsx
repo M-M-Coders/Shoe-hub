80ab337721eba00a5eb3e812088859e0fdf9f3e0
ecee <you@example.com>
2023-11-15 13:14:59 +0300

    Done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import '../styles/Login.css'
+import React from 'react';
+import '../styles/Login.css';
 
 function Login() {
+  const handleLogin = () => {
+   
+    alert('Succesfully logged in!');
+  };
+
   return (
     <div className='Login'>
-        <form className='Form'>
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" placeholder="Username" />
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Password" />
-            <input type="submit" value="Login" />
-        </form>
+      <form className='Form' onSubmit={handleLogin}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" placeholder="Username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Password" />
+        <input type="submit" value="Login" />
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
